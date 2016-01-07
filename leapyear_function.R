a70e050a76188981cb7997432ecf5a52d110a97d
Arno <arno.timmer@hotmail.com>
2016-01-07 15:55:56 +0100

    added some comments

--- leapyear_function.R
+++ leapyear_function.R
@@ -1,6 +1,11 @@
+# arno timmer
+# 07/01/2016
 leapyear <- function(year){
+	# check if the input is a number
 	if (is.numeric(year)){
+		# set the lower boundry from when leapyear was used
 		if (year >= 1582) {
+			# actual algorithm checking if we have a leapyear
 		if (year %% 4 != 0) {print('it is a common year')} 
 			else if ( year %% 100 != 0) {print('it is a leap year')} 
 				else if (year %% 400 != 0) {print('it is a common year')} 

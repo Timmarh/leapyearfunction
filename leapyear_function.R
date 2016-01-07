21444079799e58cc3174d7c88ef8020504d3a892
Arno <arno.timmer@hotmail.com>
2016-01-07 16:21:54 +0100

    another correction of the function, now it's more correct to what the exercise asks

--- leapyear_function.R
+++ leapyear_function.R
@@ -1,16 +1,16 @@
 # arno timmer & jan pokorn
 # 07/01/2016
-leapyear <- function(year){
+is.leapyear <- function(year){
 	# check if the input is a number
 	if (is.numeric(year)){
 		# set the lower boundry from when leapyear was used
 		if (year >= 1582) {
 			# actual algorithm checking if we have a leapyear
-		if (year %% 4 != 0) {print('it is a common year')} 
-			else if ( year %% 100 != 0) {print('it is a leap year')} 
-				else if (year %% 400 != 0) {print('it is a common year')} 
-		else {print('it is a leap year')}
+		if (year %% 4 != 0) {F} 
+			else if ( year %% 100 != 0) {T} 
+				else if (year %% 400 != 0) {F} 
+		else {T}
 		}
-		else {print ('We didn have leapyears yet then')}}
-	else {print ('the year is not a number')}
+		else {stop('We didn have leapyears yet then')}}
+	else {stop('the year is not a number')}
 	}

d69b4b343a828c4cc2719a01c6d512c3f61bc98a
Arno <arno.timmer@hotmail.com>
2016-01-07 15:51:06 +0100

    correction of the function, it was incorrect now it's correct

--- leapyear_function.R
+++ leapyear_function.R
@@ -1,8 +1,11 @@
 leapyear <- function(year){
-	if (year %% 4 == 0) {print('it is a common year')} 
-	else if ( year %% 100 != 0) {print('it is a leap year')} 
-	else if (year %% 400 != 0) {print('it is a common year')} 
-	else {print('it is a leap year')}
-}
-
-leapyear(2000)
+	if (is.numeric(year)){
+		if (year >= 1582) {
+		if (year %% 4 != 0) {print('it is a common year')} 
+			else if ( year %% 100 != 0) {print('it is a leap year')} 
+				else if (year %% 400 != 0) {print('it is a common year')} 
+		else {print('it is a leap year')}
+		}
+		else {print ('We didn have leapyears yet then')}}
+	else {print ('the year is not a number')}
+	}
